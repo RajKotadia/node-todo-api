8ebd0411531fd17646324f924f126cc90b80cfd5
RajKotadia <rajkotadia15@gmail.com>
2020-06-29 23:59:23 +0530

    :sparkles: Added GET /todo/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const mongoose = require('./db/mongoose');
 const Todo = require('./models/Todo');
@@ -31,6 +32,26 @@ app.get('/todos', (req, res) => {
     Todo.find()
         .then((todos) => {
             res.send({ todos });
+        })
+        .catch((err) => {
+            res.status(400).send(err);
+        });
+});
+
+// get todo with given ID
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send({});
+    }
+
+    Todo.findById(id)
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send({});
+            }
+            res.send({ todo });
         })
         .catch((err) => {
             res.status(400).send(err);
