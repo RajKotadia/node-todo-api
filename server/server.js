d3d045e02b4efd7ee78cfb80584e50843586f7e0
RajKotadia <rajkotadia15@gmail.com>
2020-06-29 21:41:30 +0530

    Added POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const mongoose = require('./db/mongoose');
 const Todo = require('./models/Todo');
 const User = require('./models/User');
+
+const app = express();
+
+// express middleware
+app.use(bodyParser.json());
+
+// express routes
+// create todo handler
+app.post('/todos', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text,
+    });
+
+    todo.save()
+        .then((doc) => {
+            res.send(doc);
+        })
+        .catch((err) => {
+            res.status(400).send(err);
+        });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
