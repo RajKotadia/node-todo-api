4fcda94e287297c6f25b8477d3f2d22084dae448
RajKotadia <rajkotadia15@gmail.com>
2020-06-30 12:00:13 +0530

    :sparkles: Added DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -58,6 +58,29 @@ app.get('/todos/:id', (req, res) => {
         });
 });
 
+// delete a todo of given ID
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    // Validate the id - if invalid send 404
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send({});
+    }
+
+    // for valid id find by id and delete
+    Todo.findByIdAndDelete(id)
+        .then((todo) => {
+            // if no such todo with gievn id
+            if (!todo) {
+                return res.status(404).send();
+            }
+            res.send(todo);
+        })
+        .catch((err) => {
+            res.status(400).send(err);
+        });
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
